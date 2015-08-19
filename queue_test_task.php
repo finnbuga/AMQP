65dc8bb11bf2fb5ec92feac7e27d2976d4b5b9f2
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2015-08-19 21:30:31 +0300

    Added comments. Refactored.

--- queue_test_task.php
+++ queue_test_task.php
@@ -22,13 +22,22 @@ define('TOKEN',        'florin');
 $connection = new AMQPConnection(HOST, PORT, USER, PASS);
 $channel = $connection->channel();
 
-
+/**
+ * Processes the message sent by the server.
+ *
+ * It reads loan data from the message,
+ * then calculates the loan interest
+ * and finally publishes a new message with the extra loan data.
+ *
+ * @param $input_msg Object containing the message body which contains loan information of the form: {sum: 123, days: 5}
+ */
 $callback = function($input_msg) {
   $input = json_decode($input_msg->body);
   
   $loan = new Loan($input->sum, $input->days);
   $loan->calculate_interest();
 
+  // Publish a new message with extra loan data.
   $output = array(
     'sum' => $loan->sum,
     'days' => $loan->days,
@@ -36,15 +45,14 @@ $callback = function($input_msg) {
     'totalSum' => $loan->sum + $loan->interest,
     'token' => TOKEN
   );
-  
-  echo json_encode($output), "\n";
-  global $channel;
   $output_msg = new AMQPMessage(json_encode($output));
+  global $channel;
   $channel->basic_publish($output_msg, '', OUTPUT_QUEUE);
 };
 
 $channel->basic_consume(INPUT_QUEUE, '', false, true, false, false, $callback);
 
+// Wait for new messages from the server
 while(count($channel->callbacks)) {
     $channel->wait();
 }
